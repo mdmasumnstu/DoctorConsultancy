0369cf445dfdd89dc8ef27afd80f58e1b67ac422
Md. Masum <151396562+mdmasumnstu@users.noreply.github.com>
2025-08-30 11:20:11 +0600

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault(); // ফর্ম সাবমিট থামানো (ডেমো জন্য)
+    alert("Your query has been submitted successfully!");
+});
